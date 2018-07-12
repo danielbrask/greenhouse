7cb4f0e23bd8a1da40611d41e464eb3fb8aa4d71
fredrha <fredrik.hansson@alten.se>
2018-07-12 11:48:15 +0200

    Extracted code from loop into programs. Added code for handlig different progras into the loop. Functionality for choosing program is not yet implemented

--- greenhouse/greenhouse.ino
+++ greenhouse/greenhouse.ino
@@ -2,13 +2,12 @@
 #include <SeeedOLED.h>
 #include <EEPROM.h>
 #include "Arduino.h"
-#include "relay.h"
+#include <relay.h>
 #include "DHT.h"
 #include "SI114X.h"
 #include "soil_moisture_sensor.h"
+#include "GroveEncoder.h"
 #include "water_flow_sensor.h"
-#include "RotaryEncoder.h"
-
 
 const int DHTPIN = 0xA0;    // what pin we're connected to
 const int MoisturePin = 0xA1;
@@ -19,13 +18,25 @@ const int EncoderPin2 = 0x4;
 const int WaterflowPin = 0x5;
 const int RelayPin = 0x6;
 
+char buffer[40];
+
 // counters for test
 bool isPumpOn = false;
 bool ButtonClicked = false;
 int clicks = 0;
 int encodercntr = 0;
 
-RotaryEncoder myEncoder(EncoderPin1, EncoderPin2);
+//Variables for displaying information on screen
+String desription = "";
+float value = 0;
+
+const int programInt = 0; // remove this after defining the proper way of handling programs
+
+void myCallback(int newValue) {
+  encodercntr++;
+}
+
+GroveEncoder myEncoder(EncoderPin1, &myCallback);
 Relay myPump(RelayPin);
 SoilMoistureSensor moistureSensor(MoisturePin);
 SI114X SI1145 = SI114X();
@@ -51,9 +62,9 @@ void setup() {
   pinMode(ButtonPin, INPUT);
   attachInterrupt(0,ButtonClick,FALLING);
 
-  pinMode(EncoderPin1, INPUT);
+  //pinMode(EncoderPin1, INPUT);
   pinMode(EncoderPin2, INPUT);
-  attachInterrupt(1,EncoderRotate,RISING);
+  //attachInterrupt(1,EncoderRotate,RISING);
 
   pinMode(WaterflowPin, INPUT);
   pinMode(RelayPin, OUTPUT);
@@ -66,38 +77,31 @@ void setup() {
 }
 
 void loop() {
-  myEncoder.tick();
+  //myPump.off();
 
+  // Read from all sensors
   auto MoisHumidity = humiditySensor.readHumidity();
   auto WaterflowRate = waterFlowSensor.measure_flow_rate();
   auto _vis = SI1145.ReadVisible();
   auto _ir = SI1145.ReadIR();
   auto _uv = SI1145.ReadUV();
 
-  displayText("Moisture: ",(int)(MoisHumidity) ,1 ,0 );
-  displayText("Waterflow: ", WaterflowRate, 4, 0);
-  displayText("Sun: ", (int)_vis, 5, 0);
-  displayText("IR: ", (int)_ir, 6, 0);
-  displayText("UV: ", (int)_uv, 7, 0);
-  displayText("Clicks: ", 2, 0);
-  
-  int _pos = myEncoder.getPosition();
-  //int _dir = 0;//myEncoder.getDirection();
-  displayText("Enc: ", _pos, 3, 0);
-  //displayText("Dir: ", _dir, 3, 9);
-  if (ButtonClicked == true) {
-    if(isPumpOn == false){
-      myPump.on();
-      isPumpOn = !isPumpOn;
-    } else if(isPumpOn == true) {
-      myPump.off();
-      isPumpOn = !isPumpOn;
-    }
-    displayText("Clicks: ", clicks, 2, 0);
-    clicks++;
-    ButtonClicked = false;
-  }
+ String str = "abs";
 
+  
+
+  //Code for running the different programs
+  switch(programInt != 0){
+      case 0:{
+    overviewProgram(MoisHumidity, WaterflowRate, _vis, _ir, _uv);
+      }
+      case 1:{
+        chilliProgram();
+      }
+      case 2:{
+        carrotProgram();
+      }
+    }
 }
 
 /**
@@ -114,10 +118,20 @@ void displayText(const char* description, int value, int xCoordinate, int yCoord
   SeeedOled.putString(str.c_str());
 }
 
-void displayText(const char* description, int xCoordinate, int yCoordinate) {
-  SeeedOled.setTextXY(xCoordinate, yCoordinate);         //Set the cursor to Xth Page, Yth Column
-  SeeedOled.putString(description);
-}
+// void displayText(const char* description, int xCoordinate, int yCoordiate) {
+//   SeeedOled.setTextXY(xCoordinate, yCoordinate);         //Set the cursor to Xth Page, Yth Column
+//   SeeedOled.putString(description);
+// }
+
+// class GrooveButton
+// {
+//   GrooveButton(int pin, void (*optionalCallBack)(int));
+//   public:
+//     bool isButtonPressed();
+//     void clearButtonPress();
+//   private:
+//     bool buttonPressedFlag;
+// }
 
 void ButtonClick() {
 
@@ -129,21 +143,36 @@ void ButtonClick() {
   }
 }
 
-// void EncoderRotate() {
+//Shows readings from all sensors and starts pump with button
+void overviewProgram(int MoisHumidity, int WaterflowRate, int _vis, int _ir, int _uv){
+
+  displayText("Moisture: ",(int)(MoisHumidity) ,1 ,0 );
+
+if (ButtonClicked == true) {
+    if(isPumpOn == false){
+      myPump.on();
+      isPumpOn = !isPumpOn;
+    } else if(isPumpOn == true) {
+      myPump.off();
+      isPumpOn = !isPumpOn;
+    }
+    displayText("Clicks: ", clicks, 2, 0);
+    clicks++;
+    ButtonClicked = false;
+  }
+
+  displayText("Encoder: ", encodercntr, 3, 0);
+  displayText("Waterflow: ", WaterflowRate, 4, 0);
+  displayText("Sun: ", (int)_vis, 5, 0);
+  displayText("IR: ", (int)_ir, 6, 0);
+  displayText("UV: ", (int)_uv, 7, 0);
   
-//     if(digitalRead(EncoderPin1) == 1) {
-//         delay(10);
-//         if(digitalRead(EncoderPin1) == 1) {
-//             if(EncoderFlag == 0) {
-//                 EncoderFlag = 1;
-//                 if(digitalRead(EncoderPin2) == 1) {
-//                     encodercntr++;
-//                 } else {
-//                     encodercntr--;
-//                 }
-//            }
-//         } else {
-//         }
-//     } else {
-//     }
-// }
+  }
+
+//Method to be used by the loop for handling chilli plants
+void chilliProgram(){
+  }
+  
+//Method to be used by the loop for handling chilli plants
+void carrotProgram(){
+  }
